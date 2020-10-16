74f6cfae75426f3c48dbe208b024c0d2e23aa017
EgorFabrichkin <70843062+EgorFabrichkin@users.noreply.github.com>
2020-10-16 18:43:52 +0300

    fixed miskate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 module.exports = function reverse (n) {
     let arr = n.toString().split('');
     let num = [];
-
     for (let i = arr.length; i >= 0 ; i--) {
         num.push(arr[i]);
     }
-    return parseInt(num.join(''));
+    let result = parseInt(num.join(''));
+    
+    return result;
 }
