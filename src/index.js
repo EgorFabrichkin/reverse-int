c5c0c49d06144f3de17d7292610bcb234d35e60f
EgorFabrichkin <70843062+EgorFabrichkin@users.noreply.github.com>
2020-10-16 17:52:35 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = n.toString().split('');
+    let num = [];
+
+    for (let i = arr.length; i >= 0 ; i--) {
+        num.push(arr[i]);
+    }
+    return parseInt(num.join(''));
 }
